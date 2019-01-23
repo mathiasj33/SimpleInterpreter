a4ce8b3d2dd6c420e8b733e013c14d39c28f1733
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2019-01-23 20:50:34 +0100

    Refactored __str__ of identifiers.

--- src/syntaxtree.py
+++ src/syntaxtree.py
@@ -91,7 +91,7 @@ class Identifier:
 			return True and self.name == other.name
 
 	def __str__(self):
-		return 'Identifier({})'.format(str(self.name))
+		return 'Identifier(\'{}\')'.format(str(self.name))
 
 	def __repr__(self):
 		return str(self)
